e2260ef6f8436687d7d661e917aa8e79c21c3e73
James Sangalli <j.l.sangalli@gmail.com>
2020-11-17 13:59:05 +1100

    read from server

--- index.js
+++ index.js
@@ -2,11 +2,26 @@ const Ethers = require('ethers');
 const provider = new Ethers.providers.Web3Provider(web3.currentProvider);
 const { ERC20, ERC721 } = require("./ABI");
 const request = require("superagent");
+const serverQuery = "https://dApp-panel-api.herokuapp.com/ethereum-interactions"
+let account = "";
 
 $(() => {
+
     window.ethereum.enable().then((accounts) => {
         account = accounts[0];
-        chainId = parseInt(window.ethereum.chainId);
+        const _ = init();
     }).catch(console.error);
+
+    async function init() {
+        const network = await provider.getNetwork();
+        const query = `${serverQuery}/${network}/${account}`;
+        request.get(query).then((result) => {
+            $("#ethTransfers").text(result.body.mostEthTransfersTo);
+            $("#mostUsedContracts").text(result.body.mostUsedContracts);
+            $("#contractsYouCreated").text(result.body.contractsYouCreated);
+            $("#mostCalledFunctions").text(result.body.mostCalledFunctions);
+        }).catch(console.error);
+    }
+
 });
 

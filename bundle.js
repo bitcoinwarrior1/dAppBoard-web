51af36b545c9c6d3aff748601788d40e9a21611a
James Sangalli <j.l.sangalli@gmail.com>
2021-01-03 23:04:10 +1100

    WIP write basic concept in code of an ETH paywall with L2 ZkSync

--- bundle.js
+++ bundle.js
@@ -42,6 +42,7 @@ const { ERC20, ERC721 } = require("./ABI");
 const request = require("superagent");
 const serverQuery = "https://dApp-panel-api.herokuapp.com/ethereum-interactions";
 let account = "";
+const feeToAccessAPI = ethers.utils.formatUnits(0.00001, "ether");
 const deploymentPrefix = "60606040";
 const { getEtherscanAddressPage, getEtherScanContractPage } = require("./helpers");
 
@@ -53,12 +54,27 @@ $(() => {
         init();
     }).catch(console.error);
 
+    //TODO form this dynamically according to the user's account
+    function getPaywallPayload() {
+        return "Transfer 0.001 ETH\n" +
+            "To: 0xfe6d4bc2de2d0b0e6fe47f08a28ed52f9d052a02\n" +
+            "Nonce: 2\n" +
+            "Fee: 0.00000475 ETH\n" +
+            "Account Id: 6084"; //TODO does this change via the recipient
+    }
+
     function init() {
         provider.getNetwork().then((network) => {
-            const query = `${serverQuery}/${network.name}/${account}`;
-            request.get(query).then((result) => {
-                render(result.body);
-            }).catch(console.error);
+            const paywallPayload = getPaywallPayload();
+            ethers.connect(provider.getSigner()).sign(paywallPayload).then((signature) => {
+                const paywallObj = {};
+                paywallObj.payload = paywallPayload;
+                paywallObj.signature = signature;
+                const query = `${serverQuery}/${network.name}/${account}/${paywallObj}`;
+                request.get(query).then((result) => {
+                    render(result.body);
+                }).catch(console.error);
+            }).catch(alert("you must pay the paywall to access this service"));
         }).catch(console.error);
     }
 
